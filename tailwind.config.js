07011ee70ee6564b0f5a9dc8352220ef5a0c5707
distinctharsh <distinctharsh@gmail.com>
2026-09-07 17:31:18 +0530

    Tailwind issue fixed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/Views/**/*.php",
+        "./app/Views/**/*.html",
+        "./public/**/*.php",
+        "./public/**/*.html",
+        "./public/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
